6eea2c6fae95a7abbc4c153f5d1d2ff9198acc74
Shan Basnayake <shanbasnayake@hotmail.com>
2024-03-20 01:55:21 +0530

    small calender

--- src/context/ContextWrapper.jsx
+++ src/context/ContextWrapper.jsx
@@ -9,14 +9,23 @@ import dayjs from "dayjs";
 
 export default function ContextWrapper(props) {
     const [monthIndex, setMonthIndex] = useState(dayjs().month());
-
-
+    const [smallCalendarMonth, setSmallCalendarMonth] = useState(null);
+    const [daySelected, setDaySelected] = useState(dayjs());
+    useEffect(() => {
+        if (smallCalendarMonth !== null) {
+          setMonthIndex(smallCalendarMonth);
+        }
+      }, [smallCalendarMonth]);
   
     return (
       <GlobalContext.Provider 
         value={{
         monthIndex,
         setMonthIndex,
+        smallCalendarMonth,
+        setSmallCalendarMonth,
+        daySelected,
+        setDaySelected,
         }}
         >
 
